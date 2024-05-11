0430e7f0b664fdb3e6464777f9a0ac430da658b8
gimdonghyeon <haroya01@naver.com>
2024-05-11 12:32:34 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/hubo/gillajabi/HuboApplication.java
@@ -0,0 +1,13 @@
+package com.hubo.gillajabi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuboApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuboApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hubo/gillajabi/HuboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hubo.gillajabi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuboApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
