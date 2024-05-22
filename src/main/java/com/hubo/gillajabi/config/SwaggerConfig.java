64a68b4950b4babc4efd5f696c912546b6330cd3
gimdonghyeon <haroya01@naver.com>
2024-05-22 23:13:52 +0900

    feat: swagger 추가

---
+++ src/main/java/com/hubo/gillajabi/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.hubo.gillajabi.config;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+@OpenAPIDefinition(
+        info = @Info(title = "Hubo API 명세서",
+                description = "Hubo API 명세서",
+                version = "v0.0.1"))
+@SecurityScheme(
+        name = "JWT Auth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi api() {
+        return GroupedOpenApi.builder()
+                .group("api")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+}
