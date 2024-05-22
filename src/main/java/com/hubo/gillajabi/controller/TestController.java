8069e3b2a81436bb183b48d6ce68da9d1b30b0fd
gimdonghyeon <haroya01@naver.com>
2024-05-22 23:19:20 +0900

    feat: 핑 테스트 api 추가

---
+++ src/main/java/com/hubo/gillajabi/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.hubo.gillajabi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/test")
+public class TestController {
+
+    @GetMapping("/hello")
+    public String test() {
+        return "world";
+    }
+}
