5e8c68fe237f16b6429cd47c9b00d6b45eff8bbf
gimdonghyeon <haroya01@naver.com>
2024-05-30 23:03:53 +0900

    feat: ApiProperties 주입을 위한 RoadProperties 추가

---
+++ src/main/java/com/hubo/gillajabi/crawl/infrastructure/config/RoadProperties.java
@@ -0,0 +1,26 @@
+package com.hubo.gillajabi.crawl.infrastructure.config;
+
+import com.hubo.gillajabi.crawl.domain.constant.ApiProperties;
+import com.hubo.gillajabi.crawl.domain.constant.CityName;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Configuration
+@ConfigurationProperties(prefix = "road")
+@Slf4j
+@Getter
+public class RoadProperties {
+
+    private final Map<CityName, ApiProperties> roadEndpoints = new HashMap<>();
+
+    public ApiProperties getEndpoint(CityName cityName) {
+        return roadEndpoints.get(cityName);
+    }
+}
