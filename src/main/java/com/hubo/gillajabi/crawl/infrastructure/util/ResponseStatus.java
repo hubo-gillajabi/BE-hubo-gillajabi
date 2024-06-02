381ce5e7ff14b74b0f8da3294cc55ee9807b8133
gimdonghyeon <haroya01@naver.com>
2024-06-02 14:34:10 +0900

    refactor: ResponseStatus 성공 조건 변경

--- src/main/java/com/hubo/gillajabi/crawl/infrastructure/util/ResponseStatus.java
+++ src/main/java/com/hubo/gillajabi/crawl/infrastructure/util/ResponseStatus.java
@@ -5,7 +5,6 @@ public enum ResponseStatus {
     NO_MANDATORY_REQUEST_PARAMETERS_ERROR(11, "필수 요청 파라메터가 없음"),
     TEMPORARILY_DISABLE_THE_SERVICEKEY_ERROR(21, "일시적으로 사용할 수 없는 서비스키"),
     UNSIGNED_CALL_ERROR(33, "서명되지 않은 호출"),
-    SUCCESS(200, "성공"),
     OK(0000, "OK"),  // 성공 응답 코드 추가
     UNKNOWN_ERROR(-1, "알 수 없는 에러");
 
