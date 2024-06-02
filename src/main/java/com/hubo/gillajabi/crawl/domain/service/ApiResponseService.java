eaa6163fbd170df81e13567f5ca5372d31bae90d
gimdonghyeon <haroya01@naver.com>
2024-06-03 00:17:40 +0900

    feat: ApiResponseService 추가

---
+++ src/main/java/com/hubo/gillajabi/crawl/domain/service/ApiResponseService.java
@@ -0,0 +1,33 @@
+package com.hubo.gillajabi.crawl.domain.service;
+
+import com.hubo.gillajabi.crawl.domain.entity.CrawlApiResponse;
+import com.hubo.gillajabi.crawl.domain.repository.CrawlApiResponseRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+public class ApiResponseService {
+
+    private final CrawlApiResponseRepository crawlApiResponseRepository;
+
+    public void saveApiResponse(String apiUrl, String response) {
+        log.info("apiUrl: {}, response: {}", apiUrl, response);
+        CrawlApiResponse crawlApiResponse = CrawlApiResponse.builder()
+                .requestUrl(apiUrl)
+                .response(response)
+                .build();
+        crawlApiResponseRepository.save(crawlApiResponse);
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<CrawlApiResponse> findByRequestUrl(String string) {
+        return crawlApiResponseRepository.findByRequestUrl(string);
+    }
+}
