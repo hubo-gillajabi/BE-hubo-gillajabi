20454b701c84d65a738e1a0d7bdb3dd5b6be5392
gimdonghyeon <haroya01@naver.com>
2024-06-05 02:18:34 +0900

    feat: CityName enum 추가

--- src/main/java/com/hubo/gillajabi/crawl/domain/constant/CityName.java
+++ src/main/java/com/hubo/gillajabi/crawl/domain/constant/CityName.java
@@ -1,5 +1,16 @@
 package com.hubo.gillajabi.crawl.domain.constant;
 
 public enum CityName {
-    DURU, BUSAN
+    DURU("두루누비"),
+    BUSAN("부산");
+
+    private final String name;
+
+    CityName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
