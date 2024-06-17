4054a0b3c9aad59f65ff39eca2fa117c3d7bdf04
gimdonghyeon <haroya01@naver.com>
2024-06-17 22:50:16 +0900

    test: test 코드 수정 - CityDuruServiceTest

--- src/test/java/com/hubo/gillajabi/crawl/domain/service/duru/CityDuruServiceTest.java
+++ src/test/java/com/hubo/gillajabi/crawl/domain/service/duru/CityDuruServiceTest.java
@@ -33,32 +33,26 @@ class CityDuruServiceTest {
 
     private final FixtureMonkey fixtureMonkey = FixtureMonkey.create();
 
+    public void findByNameAndProvince() {
+        when(cityRepository.findByNameAndProvince("김해시", Province.fromValue("경남")))
+                .thenReturn(Optional.empty());
+    }
+
     @DisplayName("DuruCourseResponse.Course를 받아서 새로운 city객체를 생성하고 저장")
     @Test
     void DuruCourseResponse_Course를_받아서_새로운_city객체를_생성하고_저장() {
         // given
         List<DuruCourseResponse.Course> responseItems = fixtureMonkey.giveMeBuilder(DuruCourseResponse.Course.class)
-                .set("sigun", "경남") // sigun 필드를 유효한 값으로 설정
-                .sampleList(3);
+                .set("sigun", "경남 김해시")
+                .sampleList(1);
 
-        try (MockedStatic<CrawlResponseParserHelper> mockedParser = mockStatic(CrawlResponseParserHelper.class)) {
-            mockedParser.when(() -> CrawlResponseParserHelper.parseDuruResponseByProvince(anyString()))
-                    .thenReturn("경남");
-            mockedParser.when(() -> CrawlResponseParserHelper.parseDuruResponseByCity(anyString()))
-                    .thenReturn("도시이름");
+        findByNameAndProvince();
 
-            for (DuruCourseResponse.Course item : responseItems) {
-                when(cityRepository.findByNameAndProvince("도시이름", Province.fromValue("경남")))
-                        .thenReturn(Optional.empty());
-            }
+        // when
+        List<City> savedCities = cityDuruService.saveCity(responseItems);
 
-            // when
-            List<City> savedCities = cityDuruService.saveCity(responseItems);
-
-            // then
-            assertNotNull(savedCities);
-            assertEquals(3, savedCities.size());
-            verify(cityRepository, times(3)).save(any(City.class));
-        }
+        // then
+        assertEquals(1, savedCities.size());
+        verify(cityRepository, times(1)).save(any(City.class));
     }
 }
