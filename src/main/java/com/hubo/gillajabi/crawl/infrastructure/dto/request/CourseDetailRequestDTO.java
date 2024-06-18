b62c1b894a943c7e577335ac1f6fa6ffcc5156aa
gimdonghyeon <haroya01@naver.com>
2024-06-18 17:50:05 +0900

    feat: CourseDetailRequestDTO 추가

--- src/main/java/com/hubo/gillajabi/crawl/infrastructure/dto/request/CourseDetailRequestDTO.java
+++ src/main/java/com/hubo/gillajabi/crawl/infrastructure/dto/request/CourseDetailRequestDTO.java
@@ -18,6 +18,8 @@ public class CourseDetailRequestDTO {
     private String endPointTransport;
     private Integer totalTimeRequired;
     private String gpxPath;
+
+
     private CycleType cycleType;
 
     public static CourseDetailRequestDTO of(final DuruCourseResponse.Course item, final String startPoint, final String endPoint,
