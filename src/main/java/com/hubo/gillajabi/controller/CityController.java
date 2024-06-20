af352d62eb3e4ec5c1f8e058f12d4fa15bb54ee1
gimdonghyeon <haroya01@naver.com>
2024-06-20 18:54:25 +0900

    feat: CityController 추가 - 테스트 용 이므로 추후에 제거할 것

---
+++ src/main/java/com/hubo/gillajabi/controller/CityController.java
@@ -0,0 +1,28 @@
+package com.hubo.gillajabi.controller;
+
+import com.hubo.gillajabi.crawl.domain.entity.City;
+import com.hubo.gillajabi.crawl.infrastructure.persistence.CityRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+import java.util.Optional;
+
+//TODO: graphql 테스트 용 이므로 추후에 제거할것
+@Controller
+@RequiredArgsConstructor
+public class CityController {
+
+    private final CityRepository cityRepository;
+
+    @QueryMapping
+    public List<City> cities() {
+        return cityRepository.findAll();
+    }
+
+    @QueryMapping
+    public Optional<City> city(Long id) {
+        return cityRepository.findById(id);
+    }
+}
