d2b2374bf491377fcbdeae4d2d33a6327d2d3c25
gimdonghyeon <haroya01@naver.com>
2024-06-28 03:52:03 +0900

    refactor: RoadDuruAbstractCrawlDuruService로 이름 변경 및 public 접근 제어자 제거

--- src/main/java/com/hubo/gillajabi/crawl/domain/service/duru/RoadDuruAbstractCrawlDuruService.java
+++ src/main/java/com/hubo/gillajabi/crawl/domain/service/duru/RoadDuruAbstractCrawlDuruService.java
@@ -2,7 +2,7 @@ package com.hubo.gillajabi.crawl.domain.service.duru;
 
 import java.util.List;
 
-public abstract class RoadAbstractCrawlDuruService {
+abstract class RoadDuruAbstractCrawlDuruService {
     public abstract List<?> crawlCourse();
 
     public abstract List<?> crawlTheme();
