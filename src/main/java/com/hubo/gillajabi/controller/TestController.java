c0bc04c1aea03e38cfd00d27dce0992c8106d88c
gimdonghyeon <haroya01@naver.com>
2024-07-03 02:17:10 +0900

    feat: 테스트 컨트롤러 테스트 추가

--- src/main/java/com/hubo/gillajabi/controller/TestController.java
+++ src/main/java/com/hubo/gillajabi/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.hubo.gillajabi.controller;
 
+import com.hubo.gillajabi.login.application.annotation.MemberOnly;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/test")
 public class TestController {
 
+    @MemberOnly
     @GetMapping("/hello")
     public String test() {
         return "world";
     }
+
 }
