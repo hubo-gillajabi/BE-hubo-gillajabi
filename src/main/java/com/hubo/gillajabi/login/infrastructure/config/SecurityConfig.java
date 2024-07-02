ffab82605370ec98a12faba2af1d16c4271a38c3
gimdonghyeon <haroya01@naver.com>
2024-07-03 02:14:13 +0900

    feat: SecurityConfig 추가

---
+++ src/main/java/com/hubo/gillajabi/login/infrastructure/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.hubo.gillajabi.login.infrastructure.config;
+
+import com.hubo.gillajabi.login.infrastructure.util.JwtFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+@Configuration
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final TokenProvider tokenProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)  // CSRF 보호 비활성화
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()  // 모든 요청에 대해 접근 허용
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)  // 세션을 생성하지 않음
+                        .sessionFixation().none()  // 세션 고정 보호 비활성화
+                )
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .addFilterBefore(new JwtFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class);  // JWT 필터 추가
+
+        return http.build();
+    }
+}
