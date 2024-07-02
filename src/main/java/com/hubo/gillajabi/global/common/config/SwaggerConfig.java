2fec9f1d8c1f84f4f52651aeae837988f853e6a9
gimdonghyeon <haroya01@naver.com>
2024-07-02 21:55:53 +0900

    feat: swagger 에서 엑세스 토큰 옵션 추가

--- src/main/java/com/hubo/gillajabi/global/common/config/SwaggerConfig.java
+++ src/main/java/com/hubo/gillajabi/global/common/config/SwaggerConfig.java
@@ -1,9 +1,10 @@
 package com.hubo.gillajabi.global.common.config;
 
-
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.models.GroupedOpenApi;
@@ -13,9 +14,15 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @RequiredArgsConstructor
 @OpenAPIDefinition(
-        info = @Info(title = "Hubo API 명세서",
-                description = "Hubo API 명세서",
-                version = "v0.0.1"))
+        info = @Info(
+                title = "Hubo API 명세서",
+                description = "Hubo API 명세서를 통해 제공되는 모든 엔드포인트에 대한 상세 설명 및 인증 방법을 포함합니다.",
+                version = "v0.0.1"
+        ),
+        security = {
+                @SecurityRequirement(name = "JWT Auth"),
+        }
+)
 @SecurityScheme(
         name = "JWT Auth",
         type = SecuritySchemeType.HTTP,
