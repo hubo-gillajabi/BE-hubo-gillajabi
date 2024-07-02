4f4266b9a341196b05fbb2cea9540de6f539ebe9
gimdonghyeon <haroya01@naver.com>
2024-07-03 02:24:20 +0900

    refactor: 에러 세분화

--- src/main/java/com/hubo/gillajabi/login/infrastructure/exception/AuthException.java
+++ src/main/java/com/hubo/gillajabi/login/infrastructure/exception/AuthException.java
@@ -7,14 +7,16 @@ public class AuthException extends RuntimeException {
 
     private final int errorCode;
     private final String errorMessage;
+    private final int httpStatus;
 
-    public AuthException(int errorCode, String errorMessage) {
+    public AuthException(int errorCode, String errorMessage, int httpStatus) {
         super(errorMessage);
         this.errorCode = errorCode;
         this.errorMessage = errorMessage;
+        this.httpStatus = httpStatus;
     }
 
     public AuthException(AuthExceptionCode authExceptionCode) {
-        this(authExceptionCode.getErrorCode(), authExceptionCode.getErrorMessage());
+        this(authExceptionCode.getErrorCode(), authExceptionCode.getErrorMessage(), authExceptionCode.getHttpStatus());
     }
 }
