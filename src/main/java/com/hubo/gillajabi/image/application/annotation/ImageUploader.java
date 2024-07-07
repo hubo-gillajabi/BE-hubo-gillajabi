b0c5575427cee0f047fc2d67668481e5b98068f8
gimdonghyeon <haroya01@naver.com>
2024-07-07 23:32:02 +0900

    feat: ImageUploaderAspect 추가

---
+++ src/main/java/com/hubo/gillajabi/image/application/annotation/ImageUploader.java
@@ -0,0 +1,13 @@
+package com.hubo.gillajabi.image.application.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ImageUploader {
+
+    int count() default 1;
+}
