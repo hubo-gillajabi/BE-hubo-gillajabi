d06be9921de06ca9ceb1e533516d7cf5564a03db
gimdonghyeon <haroya01@naver.com>
2024-07-08 23:42:16 +0900

    feat: ImageUrlProvider 추가 - dto에 사용할것

---
+++ src/main/java/com/hubo/gillajabi/global/ImageUrlProvider.java
@@ -0,0 +1,9 @@
+package com.hubo.gillajabi.global;
+
+/**
+ * ImageUrlProvider
+ * 이미지가 한개일 경우 사용할것
+ */
+public interface ImageUrlProvider {
+    String getImageUrl();
+}
---
+++ src/main/java/com/hubo/gillajabi/global/ImageUrlsProvider.java
@@ -0,0 +1,11 @@
+package com.hubo.gillajabi.global;
+
+import java.util.List;
+
+/**
+ * ImageUrlProvider
+ * 이미지가 여러개인 경우 사용할것
+ */
+public interface ImageUrlsProvider {
+    List<String> ge화tImageUrls();
+}
