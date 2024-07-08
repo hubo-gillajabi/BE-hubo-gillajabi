a0bdfde1a850a77d0619499ed26fb763a16ceb71
gimdonghyeon <haroya01@naver.com>
2024-07-08 23:42:37 +0900

    feat: 올바른 이름을 사용하도록 변경

--- src/main/java/com/hubo/gillajabi/member/infrastructure/persistence/MemberRepository.java
+++ src/main/java/com/hubo/gillajabi/member/infrastructure/persistence/MemberRepository.java
@@ -3,6 +3,9 @@ package com.hubo.gillajabi.member.infrastructure.persistence;
 import com.hubo.gillajabi.member.domain.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long>{
 
+    Optional<Member> findByNickName(String nickName);
 }
