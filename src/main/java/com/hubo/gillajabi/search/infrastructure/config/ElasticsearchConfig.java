fb265aab673d9e373dd7a544ca74f9f691076d0e
gimdonghyeon <haroya01@naver.com>
2024-07-23 01:15:20 +0900

    feat: ElasticsearchConfig 추가

---
+++ src/main/java/com/hubo/gillajabi/search/infrastructure/config/ElasticsearchConfig.java
@@ -0,0 +1,33 @@
+package com.hubo.gillajabi.search.infrastructure.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+@Configuration
+@EnableElasticsearchRepositories
+@ConfigurationProperties(prefix = "elasticsearch")
+public class ElasticsearchConfig extends ElasticsearchConfiguration {
+
+    @Value("${spring.elasticsearch.uris}")
+    private String uris;
+
+    @Value("${spring.elasticsearch.username}")
+    private String username;
+
+    @Value("${spring.elasticsearch.password}")
+    private String password;
+
+    @Override
+    public ClientConfiguration clientConfiguration() {
+        return ClientConfiguration.builder()
+                .connectedTo(uris.replace("http://", ""))
+                .withBasicAuth(username, password)
+                .build();
+    }
+
+
+}
