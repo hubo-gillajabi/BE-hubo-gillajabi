b0e162be57bb2208446e11196808d1da80be99ec
gimdonghyeon <haroya01@naver.com>
2024-07-29 00:23:04 +0900

    feat: 요청및 cookie logging

---
+++ src/main/java/com/hubo/gillajabi/global/common/aop/RequestLoggingFilter.java
@@ -0,0 +1,28 @@
+package com.hubo.gillajabi.global.common.aop;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class RequestLoggingFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        logger.info("Request URI: " + request.getRequestURI());
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                logger.info("Cookie: " + cookie.getName() + " : " + cookie.getValue());
+            }
+        } else {
+            logger.info("No cookies found.");
+        }
+        filterChain.doFilter(request, response);
+    }
+}
