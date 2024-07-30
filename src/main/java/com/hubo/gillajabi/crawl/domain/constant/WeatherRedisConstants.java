4264800d60f2fd96819ae159d6881dc5255391f4
gimdonghyeon <haroya01@naver.com>
2024-07-30 14:48:01 +0900

    feat: redis 날씨 관련 기본키 생성 추가 - WeatherRedisConstants

--- src/main/java/com/hubo/gillajabi/crawl/domain/constant/WeatherRedisConstants.java
+++ src/main/java/com/hubo/gillajabi/crawl/domain/constant/WeatherRedisConstants.java
@@ -4,6 +4,7 @@ import com.hubo.gillajabi.city.domain.entity.City;
 import jakarta.annotation.Nullable;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class WeatherRedisConstants {
@@ -26,7 +27,40 @@ public class WeatherRedisConstants {
         return WEATHER_API_RESPONSE + city.getName() + ":" + dateStr + ":" + baseTime;
     }
 
+    /**
+     * redis key 생성
+     * @param city
+     * @param date
+     * 현재 날짜로 key를 생성합니다
+     * ex) 부산 -> weather:부산:20240614
+     * @return key
+     */
     public static String makeWeatherKey(final City city, final LocalDate date) {
         return makeWeatherKey(city, date, null);
+    }
+
+    /**
+     * redis key 생성
+     * @param city
+     *  현재 시간을 반올림한 시간으로 key를 생성합니다
+     *  ex) 부산 -> weather:부산:20240614:11:00
+     * @return key
+     */
+    public static String makeWeatherKey(City city) {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDate date = now.toLocalDate();
+        String dateStr = date.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+
+        // 시간을 30분 단위로 반올림
+        int hour = now.getHour();
+        int minute = now.getMinute();
+        if (minute >= 30) {
+            hour = (hour + 1) % 24;
+        }
+        minute = 0;
+
+        String timeStr = String.format("%02d%02d", hour, minute);
+
+        return WEATHER_API_RESPONSE + city.getName() + ":" + dateStr + ":" + timeStr;
     }
 }
