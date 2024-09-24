e1ef165a5ed48968c5dc83cc349a3cd9cc58788b
gimdonghyeon <haroya01@naver.com>
2024-09-24 13:48:44 +0900

    refactor: point 타입 변경

--- src/main/java/com/hubo/gillajabi/global/common/config/MongoMigrationManager.java
+++ src/main/java/com/hubo/gillajabi/global/common/config/MongoMigrationManager.java
@@ -1,14 +1,17 @@
 package com.hubo.gillajabi.global.common.config;
 
 import com.hubo.gillajabi.place.domain.constant.PlaceType;
-import com.hubo.gillajabi.place.domain.entity.Place;
+import com.hubo.gillajabi.place.domain.entity.PlaceDocument;
 import com.opencsv.exceptions.CsvException;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Component;
@@ -23,6 +26,7 @@ import com.opencsv.CSVReader;
 
 
 @Component
+@Slf4j
 public class MongoMigrationManager {
 
     @Autowired
@@ -34,47 +38,47 @@ public class MongoMigrationManager {
         Resource[] resources = resolver.getResources("classpath:db/mongo_migration/*.csv");
 
         for (Resource resource : resources) {
-            List<Place> places = readPlacesFromCsv(resource);
-            for (Place place : places) {
+            List<PlaceDocument> placeDocuments = readPlacesFromCsv(resource);
+            for (PlaceDocument placeDocument : placeDocuments) {
                 // 중복 체크 로직 추가
                 boolean exists = mongoTemplate.exists(
                         Query.query(
-                                Criteria.where("type").is(place.getType())
-                                        .and("courseId").is(place.getCourseId())
+                                Criteria.where("type").is(placeDocument.getType())
+                                        .and("courseId").is(placeDocument.getCourseId())
                         ),
-                        Place.class
+                        PlaceDocument.class
                 );
 
                 if (!exists) {
-                    mongoTemplate.save(place);
+                    mongoTemplate.save(placeDocument);
                 }
             }
-            System.out.println("mongodb data migration : " + resource.getFilename());
+            System.out.println("데이터 마이그레이션 완료: " + resource.getFilename());
         }
     }
 
-    private List<Place> readPlacesFromCsv(Resource resource) throws IOException {
-        List<Place> places = new ArrayList<>();
+    private List<PlaceDocument> readPlacesFromCsv(Resource resource) throws IOException {
+        List<PlaceDocument> placeDocuments = new ArrayList<>();
         try (CSVReader reader = new CSVReader(new InputStreamReader(resource.getInputStream()))) {
             List<String[]> rows = reader.readAll();
             rows.remove(0); // 첫줄 표 제목 제거
             for (String[] row : rows) {
-                Place place = new Place();
-                place.setLocation(new BigDecimal[]{
-                        new BigDecimal(row[0]),
-                        new BigDecimal(row[1])
-                });
-                place.setName(row[2]);
-                place.setType(PlaceType.fromKoreanName(row[3]));
-                place.setDescription(row[4]);
-                place.setImageUrl(row[5]);
-                place.setMapUrl(row[6]);
-                place.setCourseId(row[7].isEmpty() ? null : Integer.parseInt(row[7]));
-                places.add(place);
+                PlaceDocument placeDocument = new PlaceDocument();
+                placeDocument.setLocation(new GeoJsonPoint(
+                        Double.parseDouble(row[0]),
+                        Double.parseDouble(row[1])
+                ));
+                placeDocument.setName(row[2]);
+                placeDocument.setType(PlaceType.fromKoreanName(row[3]));
+                placeDocument.setDescription(row[4]);
+                placeDocument.setImageUrl(row[5]);
+                placeDocument.setMapUrl(row[6]);
+                placeDocument.setCourseId(row[7].isEmpty() ? null : Integer.parseInt(row[7]));
+                placeDocuments.add(placeDocument);
             }
         } catch (CsvException e) {
             throw new IOException("csv파싱 에러 ", e);
         }
-        return places;
+        return placeDocuments;
     }
 }
