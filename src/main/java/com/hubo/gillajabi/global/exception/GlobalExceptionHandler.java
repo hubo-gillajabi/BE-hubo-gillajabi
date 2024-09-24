c360da4bef0f5d663eba27d7040e0f43843498d8
gimdonghyeon <haroya01@naver.com>
2024-09-24 13:49:36 +0900

    feat: 북마크 관련 에러 핸들러 추가

--- src/main/java/com/hubo/gillajabi/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/hubo/gillajabi/global/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.hubo.gillajabi.global.exception;
 
+import com.hubo.gillajabi.course.infrastructure.exception.CourseBookMarkException;
 import com.hubo.gillajabi.course.infrastructure.exception.CourseException;
+import com.hubo.gillajabi.crawl.infrastructure.dto.response.AbstractApiResponse;
 import com.hubo.gillajabi.crawl.infrastructure.exception.CrawlException;
 import com.hubo.gillajabi.image.infrastructure.exception.ImageException;
 import com.hubo.gillajabi.login.infrastructure.exception.AuthException;
@@ -112,6 +114,14 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(PostException.class)
     public ResponseEntity<ExceptionResponse> handlePostException(final PostException e) {
         log.warn("포스트 오류 : {} " , e.getMessage());
+
+        return ResponseEntity.status(e.getHttpStatus())
+                .body(new ExceptionResponse(e.getErrorCode(), e.getMessage()));
+    }
+
+    @ExceptionHandler(CourseBookMarkException.class)
+    public ResponseEntity<ExceptionResponse> handleCourseBookMarkException(final CourseBookMarkException e){
+        log.warn("코스 북마크 오류 : {} " , e.getMessage());
 
         return ResponseEntity.status(e.getHttpStatus())
                 .body(new ExceptionResponse(e.getErrorCode(), e.getMessage()));
