a48e3ee29c842d0478caf414eed871eb2ada0ebd
gimdonghyeon <haroya01@naver.com>
2024-09-24 14:06:35 +0900

    refactor: photoPoint sout 제거

--- src/main/java/com/hubo/gillajabi/track/domain/entity/PhotoPoint.java
+++ src/main/java/com/hubo/gillajabi/track/domain/entity/PhotoPoint.java
@@ -3,19 +3,24 @@ package com.hubo.gillajabi.track.domain.entity;
 import com.hubo.gillajabi.global.BaseEntity;
 import com.hubo.gillajabi.image.domain.entity.ImageGpsInfo;
 import com.hubo.gillajabi.member.domain.entity.Member;
+import com.hubo.gillajabi.point.application.dto.request.UserPointRequest;
+import com.hubo.gillajabi.point.domain.entity.UserPoint;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLRestriction;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Set;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
+@SQLRestriction(value = "status != 'DELETED'")
 public class PhotoPoint extends BaseEntity {
 
     @Id
@@ -32,15 +37,25 @@ public class PhotoPoint extends BaseEntity {
 
     private String photoUrl; // 사진 URL
 
-    public static PhotoPoint createByImageGpsInfo(TrackRecord trackRecord, ImageGpsInfo imageGpsInfo) {
-        return new PhotoPoint(null, trackRecord, imageGpsInfo.getLatitude(), imageGpsInfo.getLongitude(), imageGpsInfo.getImageUploadUrl());
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_point_id")
+    private UserPoint userPoint;
+
+    public static PhotoPoint createByImageGpsInfo(final TrackRecord trackRecord, final ImageGpsInfo imageGpsInfo) {
+        return new PhotoPoint(null, trackRecord, imageGpsInfo.getLatitude(), imageGpsInfo.getLongitude(), imageGpsInfo.getImageUploadUrl(), null);
     }
 
-    public static void addTrackRecord(Set<PhotoPoint> photoPoints, TrackRecord trackRecord) {
+    public static void addTrackRecord(final Set<PhotoPoint> photoPoints, final TrackRecord trackRecord) {
         photoPoints.forEach(photoPoint -> photoPoint.addTrackRecord(trackRecord));
     }
 
-    private void addTrackRecord(TrackRecord trackRecord) {
+    public static List<PhotoPoint> createByImageUrlAndTrackRecordAndUserPoint(UserPointRequest request, TrackRecord trackRecord, UserPoint userPoint) {
+        return request.getImageUrls().stream()
+                .map(imageUrl -> new PhotoPoint(null, trackRecord, request.latitude(), request.longitude(), imageUrl, userPoint))
+                .toList();
+    }
+
+    private void addTrackRecord(final TrackRecord trackRecord) {
         this.trackRecord = trackRecord;
     }
 }
