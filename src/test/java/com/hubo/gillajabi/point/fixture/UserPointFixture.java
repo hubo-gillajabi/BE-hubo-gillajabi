350044b3cbc54d3870352d21b4c1f0f16f17d86d
gimdonghyeon <haroya01@naver.com>
2024-09-29 16:58:34 +0900

    feat: UserPointSearch 관련 e2e 테스트 추가

---
+++ src/test/java/com/hubo/gillajabi/point/fixture/UserPointFixture.java
@@ -0,0 +1,167 @@
+package com.hubo.gillajabi.point.fixture;
+
+import com.hubo.gillajabi.city.domain.entity.City;
+import com.hubo.gillajabi.city.infrastructure.persistence.CityRepository;
+import com.hubo.gillajabi.config.FixtureBase;
+import com.hubo.gillajabi.course.domain.entity.CourseBookMark;
+import com.hubo.gillajabi.course.infrastructure.persistence.CourseBookMarkRepository;
+import com.hubo.gillajabi.crawl.domain.constant.Province;
+import com.hubo.gillajabi.crawl.domain.entity.Course;
+import com.hubo.gillajabi.crawl.infrastructure.persistence.CourseRepository;
+import com.hubo.gillajabi.login.application.dto.response.TokenResponse;
+import com.hubo.gillajabi.login.domain.constant.RoleStatus;
+import com.hubo.gillajabi.login.domain.entity.MemberAuthentication;
+import com.hubo.gillajabi.login.infrastructure.security.TokenProvider;
+import com.hubo.gillajabi.member.domain.entity.Member;
+import com.hubo.gillajabi.member.infrastructure.persistence.MemberRepository;
+import com.hubo.gillajabi.point.domain.entity.UserPoint;
+import com.hubo.gillajabi.point.domain.entity.UserPointDocument;
+import com.hubo.gillajabi.point.infrastructure.persistence.UserPointDocumentRepository;
+import com.hubo.gillajabi.point.infrastructure.persistence.UserPointRepository;
+import com.hubo.gillajabi.track.domain.entity.PhotoPoint;
+import com.hubo.gillajabi.track.infrastructure.persistence.PhotoPointRepository;
+import net.bytebuddy.utility.RandomString;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.UUID;
+
+@Component
+@Profile("test")
+public class UserPointFixture extends FixtureBase {
+
+    @Autowired
+    private UserPointDocumentRepository userPointDocumentRepository;
+
+    public Course createAndLoadCourse() {
+        CourseRepository courseRepository = getRepository(CourseRepository.class);
+        String randomOriginName = UUID.randomUUID().toString().substring(0, 10);
+
+        City city = createAndLoadCity();
+
+        Course course = Course.builder()
+                .originName(randomOriginName)
+                .city(city)
+                .courseNumber(generateUniqueCourseNumber())
+                .shortDescription("shortDescription")
+                .build();
+        return courseRepository.save(course);
+    }
+
+    private String generateUniqueCourseNumber() {
+        long timestamp = Instant.now().toEpochMilli();
+        String timestampStr = String.valueOf(timestamp);
+
+        return timestampStr.substring(Math.max(0, timestampStr.length() - 10));
+    }
+
+    public City createAndLoadCity(){
+        CityRepository cityRepository = getRepository(CityRepository.class);
+
+        String randomName = UUID.randomUUID().toString().substring(0, 10);
+
+        City city = City.builder()
+                .name(randomName)
+                .province(Province.BUSAN)
+                .build();
+        return cityRepository.save(city);
+    }
+
+    public UserPoint createUserPoint(Member member) {
+        UserPointRepository userPointRepository = getRepository(UserPointRepository.class);
+        PhotoPoint photoPoint = createAndSavePhotoPoint();
+        Course course = createAndLoadCourse();
+
+        UserPoint userPoint = UserPoint.builder()
+                .member(member)
+                .content("content")
+                .latitude(BigDecimal.valueOf(37.1234))
+                .longitude(BigDecimal.valueOf(127.1234))
+                .photoPoint(Arrays.asList(photoPoint))
+                .course(course)
+                .build();
+
+        return userPointRepository.save(userPoint);
+    }
+
+    public UserPoint createUserPoint(Member member, Course course){
+        UserPointRepository userPointRepository = getRepository(UserPointRepository.class);
+        PhotoPoint photoPoint = createAndSavePhotoPoint();
+
+        UserPoint userPoint = UserPoint.builder()
+                .member(member)
+                .content("content")
+                .latitude(BigDecimal.valueOf(37.1234))
+                .longitude(BigDecimal.valueOf(127.1234))
+                .photoPoint(Arrays.asList(photoPoint))
+                .course(course)
+                .build();
+
+        System.out.println("userPointid 시발" + userPoint.getCourse().getId());
+
+        return userPointRepository.save(userPoint);
+    }
+
+    private PhotoPoint createAndSavePhotoPoint() {
+        PhotoPointRepository photoPointRepository = getRepository(PhotoPointRepository.class);
+        PhotoPoint photoPoint = PhotoPoint.builder()
+                .latitude(BigDecimal.valueOf(37.1234))
+                .longitude(BigDecimal.valueOf(127.1234))
+                .photoUrl("photo.com")
+                .build();
+        return photoPointRepository.save(photoPoint);
+    }
+
+    public UserPointDocument createUserPointDocument(Member member) {
+        UserPoint userPoint = createUserPoint(member);
+        UserPointDocument userPointDocument = UserPointDocument.createByUserPoint(userPoint);
+        return userPointDocumentRepository.save(userPointDocument);
+    }
+
+    public UserPointDocument createUserPointDocument(Member member, Course course){
+        UserPoint userPoint = createUserPoint(member, course);
+        UserPointDocument userPointDocument = UserPointDocument.createByUserPoint(userPoint);
+        return userPointDocumentRepository.save(userPointDocument);
+    }
+
+    public CourseBookMark createCourseBookMark(Member member) {
+        Course course = createAndLoadCourse();
+        return createCourseBookMark(member, course);
+    }
+
+    public CourseBookMark createCourseBookMark(Member member, Course course) {
+        CourseBookMarkRepository courseBookMarkRepository = getRepository(CourseBookMarkRepository.class);
+
+        CourseBookMark courseBookMark = CourseBookMark.builder()
+                .member(member)
+                .course(course)
+                .build();
+
+        System.out.println("courseBookMarkid 시발" + courseBookMark.getCourse().getId());
+
+        return courseBookMarkRepository.save(courseBookMark);
+    }
+
+    public UserPoint createAndLoadUserPoint(Member member, Course course) {
+        UserPointRepository userPointRepository = getRepository(UserPointRepository.class);
+        PhotoPoint photoPoint = createAndSavePhotoPoint();
+
+        UserPoint userPoint = UserPoint.builder()
+                .member(member)
+                .content("content")
+                .latitude(BigDecimal.valueOf(37.1234))
+                .longitude(BigDecimal.valueOf(127.1234))
+                .photoPoint(Arrays.asList(photoPoint))
+                .course(course)
+                .build();
+
+        return userPointRepository.save(userPoint);
+    }
+}
