f96319f2d521cfb93dc6aa2c0bf94625872833d7
gimdonghyeon <haroya01@naver.com>
2024-09-30 12:18:47 +0900

    fix: github action 환경 설정

--- src/test/java/com/hubo/gillajabi/HuboApplicationTests.java
+++ src/test/java/com/hubo/gillajabi/HuboApplicationTests.java
@@ -1,5 +1,8 @@
 package com.hubo.gillajabi;
 
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch._types.NodeStatistics;
+import co.elastic.clients.elasticsearch.nodes.Stats;
 import org.elasticsearch.client.Request;
 import org.elasticsearch.client.Response;
 import org.elasticsearch.client.RestClient;
@@ -12,21 +15,32 @@ import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+
 @SpringBootTest
 @ActiveProfiles("test")
 class HuboApplicationTests {
 
+    private static final Logger logger = LoggerFactory.getLogger(HuboApplicationTests.class);
+
     @Autowired
-    private RestClient restClient;
+    private ElasticsearchClient elasticsearchClient;
 
     @Test
-    public void testElasticsearchConnection() throws IOException {
-        assertNotNull(restClient, "RestClient is null");
-
-        Request request = new Request("GET", "/");
-        Response response = restClient.performRequest(request);
-
-        assertEquals(200, response.getStatusLine().getStatusCode(), "success");
+    public void testElasticsearchConnection() {
+        logger.info("Starting Elasticsearch connection test");
+        try {
+            var response = elasticsearchClient.info();
+            logger.info("Elasticsearch info: {}", response);
+        } catch (Exception e) {
+            logger.error("Error connecting to Elasticsearch", e);
+            throw new RuntimeException("Failed to connect to Elasticsearch", e);
+        }
     }
-
 }
