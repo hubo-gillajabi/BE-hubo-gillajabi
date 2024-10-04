cc77cd5570b9281b42741573212435bf07e78a9a
gimdonghyeon <haroya01@naver.com>
2024-10-04 11:27:13 +0900

    test: test 코드 작성

---
+++ src/test/java/com/hubo/gillajabi/point/application/presenation/UserPointControllerTest.java
@@ -0,0 +1,248 @@
+package com.hubo.gillajabi.point.application.presenation;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hubo.gillajabi.image.domain.entity.ImageUploadUrl;
+import com.hubo.gillajabi.login.application.dto.response.TokenResponse;
+import com.hubo.gillajabi.login.domain.constant.RoleStatus;
+import com.hubo.gillajabi.login.domain.entity.MemberAuthentication;
+import com.hubo.gillajabi.point.application.dto.request.UserPointRequest;
+import com.hubo.gillajabi.point.domain.entity.UserPointDocument;
+import com.hubo.gillajabi.point.domain.service.UserPointService;
+import com.hubo.gillajabi.point.fixture.UserPointFixture;
+import com.hubo.gillajabi.point.infrastructure.persistence.UserPointDocumentRepository;
+import com.navercorp.fixturemonkey.FixtureMonkey;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.redis.connection.RedisServerCommands;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Objects;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@Transactional
+@ActiveProfiles("test")
+public class UserPointControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserPointController userPointController;
+
+    @Autowired
+    private UserPointService userPointService;
+
+    @Autowired
+    private UserPointFixture userPointFixture;
+
+    @Autowired
+    private UserPointDocumentRepository userPointDocumentRepository;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @AfterEach
+    void cleanupAfterTest() {
+        mongoTemplate.getCollectionNames().forEach(collectionName -> {
+            mongoTemplate.dropCollection(collectionName);
+        });
+
+        RedisServerCommands serverCommands = Objects.requireNonNull(redisTemplate.getConnectionFactory())
+                .getConnection()
+                .serverCommands();
+        serverCommands.flushAll();
+    }
+
+    @DisplayName("사용자 포인트 저장이 성공한다")
+    @Test
+    void 사용자_포인트_저장_성공() throws Exception {
+        // given
+        String requestJson = """
+                {
+                  "content": "여기는 좋은 장소입니다.",
+                  "latitude": 37.5665,
+                  "longitude": 126.9780,
+                  "imageUrls": [
+                    "https://example.com/image1.jpg"
+                  ]
+                }
+                """.replace("\n", "").replace("\r", "");
+        ImageUploadUrl imageUploadUrl = userPointFixture.createImageUploadUrl("https://example.com/image1.jpg");
+
+        MemberAuthentication memberAuthentication = userPointFixture.createAndLoadMember("testUser", RoleStatus.USER);
+        TokenResponse tokenResponse = userPointFixture.createAndLoadTokenResponse(memberAuthentication);
+        userPointFixture.startTrack(memberAuthentication.getMember(), userPointFixture.createAndLoadCourse(), imageUploadUrl);
+
+        // when
+        mockMvc.perform(
+                        post("/api/user-point")
+                                .header("Authorization", "Bearer " + tokenResponse.accessToken())
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(requestJson))
+                .andDo(print())
+                .andExpect(status().isCreated());
+
+        // then
+        List<UserPointDocument> userPointDocuments = userPointDocumentRepository.findAll();
+        assertThat(userPointDocuments).hasSize(1);
+        assertThat(userPointDocuments.get(0).getContent()).isEqualTo("여기는 좋은 장소입니다.");
+    }
+
+    @DisplayName("사용자 포인트의 이미지가 없을경우")
+    @Test
+    void 사용자의_포인트_저장_요청에서_이미지가_없을경우_() throws Exception {
+        // given
+        String requestJson = """
+                {
+                  "content": "여기는 좋은 장소입니다.",
+                  "latitude": 37.5665,
+                  "longitude": 126.9780,
+                  "imageUrls": [
+                    "https://example.com/image1.jpg"
+                  ]
+                }
+                """.replace("\n", "").replace("\r", "");
+        MemberAuthentication memberAuthentication = userPointFixture.createAndLoadMember("testUser", RoleStatus.USER);
+        TokenResponse tokenResponse = userPointFixture.createAndLoadTokenResponse(memberAuthentication);
+
+        // when
+        String result = mockMvc.perform(
+                        post("/api/user-point")
+                                .header("Authorization", "Bearer " + tokenResponse.accessToken())
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(requestJson))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+
+        // then
+        JsonNode rootNode = objectMapper.readTree(result);
+        assertThat(rootNode.get("message").asText()).isEqualTo("유효하지 않은 이미지입니다.");
+    }
+
+    @DisplayName("트래킹 중이 아닌경우 실패")
+    @Test
+    void 사용자가_트래킹_중이_아닌_경우() throws Exception {
+        // given
+        String requestJson = """
+                {
+                  "content": "여기는 좋은 장소입니다.",
+                  "latitude": 37.5665,
+                  "longitude": 126.9780,
+                  "imageUrls": [
+                    "https://example.com/image1.jpg"
+                  ]
+                }
+                """.replace("\n", "").replace("\r", "");
+        MemberAuthentication memberAuthentication = userPointFixture.createAndLoadMember("testUser", RoleStatus.USER);
+        TokenResponse tokenResponse = userPointFixture.createAndLoadTokenResponse(memberAuthentication);
+        ImageUploadUrl imageUploadUrl = userPointFixture.createImageUploadUrl("https://example.com/image1.jpg");
+
+        // when
+        String result = mockMvc.perform(
+                        post("/api/user-point")
+                                .header("Authorization", "Bearer " + tokenResponse.accessToken())
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(requestJson))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+
+        // then
+        JsonNode rootNode = objectMapper.readTree(result);
+        assertThat(rootNode.get("message").asText()).isEqualTo("현재 트래킹중이 아닙니다.");
+    }
+
+    @DisplayName("사용자 포인트 삭제가 성공한다")
+    @Test
+    void 사용자_포인트_삭제_성공() throws Exception {
+        // given
+        MemberAuthentication memberAuthentication = userPointFixture.createAndLoadMember("testUser", RoleStatus.USER);
+        TokenResponse tokenResponse = userPointFixture.createAndLoadTokenResponse(memberAuthentication);
+        UserPointDocument userPointDocument = userPointFixture.createAndLoadUserPointDocument(memberAuthentication.getMember());
+
+        // when
+        mockMvc.perform(
+                        delete("/api/user-point/{id}", userPointDocument.getUserPointId())
+                                .header("Authorization", "Bearer " + tokenResponse.accessToken())
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+
+        // then
+        List<UserPointDocument> userPointDocuments = userPointDocumentRepository.findAll();
+        assertThat(userPointDocuments).isEmpty();
+    }
+
+    @DisplayName("사용자 포인트 삭제 시 해당 사용자의 포인트가 없을 경우 실패한다")
+    @Test
+    void 사용자_포인트_삭제시_해당_사용자의_포인트가_없을_경우_실패한다() throws Exception {
+        // given
+        MemberAuthentication memberAuthentication = userPointFixture.createAndLoadMember("testUser", RoleStatus.USER);
+        TokenResponse tokenResponse = userPointFixture.createAndLoadTokenResponse(memberAuthentication);
+
+        // when
+        String result = mockMvc.perform(
+                        delete("/api/user-point/{id}", 1L)
+                                .header("Authorization", "Bearer " + tokenResponse.accessToken())
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isNotFound())
+                .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+
+        // then
+        JsonNode rootNode = objectMapper.readTree(result);
+        assertThat(rootNode.get("message").asText()).isEqualTo("포인트를 찾을 수 없습니다.");
+    }
+
+    @DisplayName("사용자 포인트 삭제시 해당 사용자의 포인트가 아닐 경우 실패한다")
+    @Test
+    void 사용자_포인트_삭제시_해당_사용자의_포인트가_아닐_경우_실패() throws Exception {
+        // given
+        MemberAuthentication memberAuthentication = userPointFixture.createAndLoadMember("testUser", RoleStatus.USER);
+        TokenResponse tokenResponse = userPointFixture.createAndLoadTokenResponse(memberAuthentication);
+        UserPointDocument userPointDocument = userPointFixture.createAndLoadUserPointDocument(memberAuthentication.getMember());
+        MemberAuthentication otherMemberAuthentication = userPointFixture.createAndLoadMember("otherUser", RoleStatus.USER);
+        TokenResponse otherTokenResponse = userPointFixture.createAndLoadTokenResponse(otherMemberAuthentication);
+
+        // when
+        String result = mockMvc.perform(
+                        delete("/api/user-point/{id}", userPointDocument.getUserPointId())
+                                .header("Authorization", "Bearer " + otherTokenResponse.accessToken())
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isForbidden())
+                .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+
+        // then
+        JsonNode rootNode = objectMapper.readTree(result);
+        assertThat(rootNode.get("message").asText()).isEqualTo("포인트 소유자가 아닙니다.");
+    }
+}
+
--- src/test/java/com/hubo/gillajabi/point/fixture/UserPointFixture.java
+++ src/test/java/com/hubo/gillajabi/point/fixture/UserPointFixture.java
@@ -16,12 +16,23 @@ import com.hubo.gillajabi.login.domain.entity.MemberAuthentication;
 import com.hubo.gillajabi.login.infrastructure.security.TokenProvider;
 import com.hubo.gillajabi.member.domain.entity.Member;
 import com.hubo.gillajabi.member.infrastructure.persistence.MemberRepository;
+import com.hubo.gillajabi.point.application.dto.request.UserPointRequest;
 import com.hubo.gillajabi.point.domain.entity.UserPoint;
 import com.hubo.gillajabi.point.domain.entity.UserPointDocument;
+import com.hubo.gillajabi.point.domain.service.UserPointSearchService;
+import com.hubo.gillajabi.point.domain.service.UserPointService;
 import com.hubo.gillajabi.point.infrastructure.persistence.UserPointDocumentRepository;
 import com.hubo.gillajabi.point.infrastructure.persistence.UserPointRepository;
+import com.hubo.gillajabi.track.application.dto.request.TrackStartRequest;
+import com.hubo.gillajabi.track.application.dto.response.StartTrackResponse;
 import com.hubo.gillajabi.track.domain.entity.PhotoPoint;
+import com.hubo.gillajabi.track.domain.entity.TrackRecord;
+import com.hubo.gillajabi.track.domain.entity.TrackStatus;
+import com.hubo.gillajabi.track.domain.service.TrackService;
 import com.hubo.gillajabi.track.infrastructure.persistence.PhotoPointRepository;
+import com.hubo.gillajabi.track.infrastructure.persistence.TrackRecordRepository;
+import com.hubo.gillajabi.track.infrastructure.persistence.TrackStatusRepository;
+import com.hubo.gillajabi.track.infrastructure.timer.TrackStatusTimer;
 import net.bytebuddy.utility.RandomString;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +40,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.sound.midi.Track;
 import java.math.BigDecimal;
 import java.time.Instant;
 import java.time.LocalDateTime;
@@ -37,12 +49,24 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
+import static com.hubo.gillajabi.track.domain.entity.TrackStatus.createByMemberAndTrackId;
+
 @Component
 @Profile("test")
 public class UserPointFixture extends FixtureBase {
 
     @Autowired
     private UserPointDocumentRepository userPointDocumentRepository;
+
+    @Autowired
+    private UserPointService userPointService;
+
+    @Autowired
+    private TrackStatusTimer trackStatusTimer;
+
+    @Autowired
+    private TrackService trackService;
+
 
     public Course createAndLoadCourse() {
         CourseRepository courseRepository = getRepository(CourseRepository.class);
@@ -173,5 +197,40 @@ public class UserPointFixture extends FixtureBase {
     }
 
     //startTrack
+    public void startTrack(Member member, Course course, ImageUploadUrl imageUploadUrl){
+        String userName = member.getNickName();
+        TrackRecordRepository trackRecordRepository = getRepository(TrackRecordRepository.class);
+        TrackStatusRepository trackStatusRepository = getCrudRepository(TrackStatusRepository.class);
 
+        final TrackRecord trackRecord = TrackRecord.createByMember(member, course);
+        trackRecordRepository.save(trackRecord);
+
+        final TrackStatus trackStatus = TrackStatus.createByMemberAndTrackId(member, trackRecord.getId());
+        trackStatusRepository.save(trackStatus);
+
+        trackStatusTimer.startTimerForMember(userName);
+
+//        UserPointRequest userPointRequest = new UserPointRequest( "content" ,BigDecimal.valueOf(37.1234), BigDecimal.valueOf(127.1234),  List.of(imageUploadUrl.getImageUploadUrl()));
+//        userPointService.saveUserPoint(userName, userPointRequest);
+    }
+
+    public UserPointDocument createAndLoadUserPointDocument(Member member) {
+        UserPointRepository userPointRepository = getRepository(UserPointRepository.class);
+        PhotoPoint photoPoint = createAndSavePhotoPoint();
+        Course course = createAndLoadCourse();
+
+        UserPoint userPoint = UserPoint.builder()
+                .member(member)
+                .content("content")
+                .latitude(BigDecimal.valueOf(37.1234))
+                .longitude(BigDecimal.valueOf(127.1234))
+                .photoPoint(Arrays.asList(photoPoint))
+                .course(course)
+                .build();
+
+        userPointRepository.save(userPoint);
+
+        UserPointDocument userPointDocument = UserPointDocument.createByUserPoint(userPoint);
+        return userPointDocumentRepository.save(userPointDocument);
+    }
 }
