b894f0333e5afaed1dc3004edac6b1d8c521d8bb
gimdonghyeon <haroya01@naver.com>
2024-10-06 23:04:06 +0900

    refactor: quest 관련 코드 제거

--- src/main/java/com/hubo/gillajabi/quest/infrastructure/persistence/MainQuestStatusRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hubo.gillajabi.quest.infrastructure.persistence;
-
-import com.hubo.gillajabi.quest.domain.entity.MainQuestStatus;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MainQuestStatusRepository extends JpaRepository<MainQuestStatus, Long> {
-}
